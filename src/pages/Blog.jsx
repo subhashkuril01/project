e53d6e5fba9bd559e64ea6638eb15cc2a88b6cb9
subhash <subhashkuril2004@gmail.com>
2025-09-09 01:25:21 +0530

    typo

--- src/pages/Blog.jsx
+++ src/pages/Blog.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import Subheader from '../Components/Subheader/Subheader'
-import BlogContent from '../Components/Blogcontent/Blogcontent'
+import BlogContent from '../Components/BlogContent/BlogContent'
 import Footer from '../Components/Footer/Footer'
 
 const Blog = () => {
